c24d00b453fc2d459796bed916af62510d6611e7
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-02 17:24:31 +0330

    tried to connect to database

--- src/main/java/hangman/DatabaseManager.java
+++ src/main/java/hangman/DatabaseManager.java
@@ -5,4 +5,28 @@ import java.sql.*;
 // Use JDBC to connect to your database and run queries
 
 public class DatabaseManager {
+    public static void main(String[] args)  {
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/Hangman";
+        String username = "postgres";
+        String password = "@Dino3386669";
+
+
+        // Register the PostgreSQL driver
+
+
+        // Connect to the database
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // Perform desired database operations
+
+            // Close the connection
+            connection.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
