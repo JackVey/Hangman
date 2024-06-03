29e1436ec5e5fd02f6222dfd37f775317d13b198
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-04 01:02:16 +0330

    finished programing User class

--- src/main/java/hangman/User.java
+++ src/main/java/hangman/User.java
@@ -1,4 +1,17 @@
 package hangman;
 
 public class User {
+    public static String username;
+
+    User(String username){
+        User.username = username;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void logout(){
+        username = "";
+    }
 }
