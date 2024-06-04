5032402e8459dfee585960ebc22741e3136e46de
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-05 02:53:00 +0330

    finished programing game page

--- src/main/java/hangman/GamePlay.java
+++ src/main/java/hangman/GamePlay.java
@@ -10,6 +10,7 @@ public class GamePlay {
     private Instant endTime;
     private final String word;
     private int wrongGuesses;
+    private int wordsRevealed;
     GamePlay(String username, String word){
         this.gameInfo = new GameInfo();
         this.gameInfo.setWord(word);
@@ -20,20 +21,20 @@ public class GamePlay {
         startTime = Instant.now();
     }
 
+    public int getWrongGuesses() {
+        return wrongGuesses;
+    }
+
     public void pointEndTime(){
         endTime = Instant.now();
     }
-
-    public void setDuration(){
-        gameInfo.setTime((int) Duration.between(startTime, endTime).toSeconds());
-    }
-
-    public void addWrongGuess(){
-        wrongGuesses ++;
-    }
-
-    public boolean checkLoose(){
-        return wrongGuesses == 6;
+    public void setDuration(){ gameInfo.setTime((int) Duration.between(startTime, endTime).toSeconds()); }
+    public void addWrongGuess(){ wrongGuesses ++; }
+    public void addRightGuess(){ wordsRevealed++; }
+    public boolean checkLoose(){ return wrongGuesses == 6; }
+    public boolean checkWin(){ return wordsRevealed == wordLength();}
+    public String getWord() {
+        return word;
     }
 
     public ArrayList<Integer> findIndexesOfLetters(String ch){
@@ -41,8 +42,27 @@ public class GamePlay {
         String tempWord = word.toUpperCase();
         while (tempWord.contains(ch)){
             indexes.add(tempWord.indexOf(ch));
-            tempWord.replace(ch, "*");
+            tempWord = tempWord.replaceFirst(ch, "*");
+            addRightGuess();
         }
         return indexes;
+    }
+    public void loose(){
+        pointEndTime();
+        setDuration();
+        gameInfo.setWrongGuesses(wrongGuesses);
+        gameInfo.setWin(false);
+        DatabaseManager.writeGameInfoData(gameInfo);
+    }
+    public void win(){
+        pointEndTime();
+        setDuration();
+        gameInfo.setWrongGuesses(wrongGuesses);
+        gameInfo.setWin(true);
+        DatabaseManager.writeGameInfoData(gameInfo);
+    }
+
+    public int wordLength(){
+        return word.length();
     }
 }
