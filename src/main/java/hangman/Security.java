b92eef6a0960a59ee1229a254ca23079b7383234
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-04 18:00:30 +0330

    implemented validateLoginInfo method

--- src/main/java/hangman/Security.java
+++ src/main/java/hangman/Security.java
@@ -1,4 +1,14 @@
 package hangman;
 
 public class Security {
+    public static boolean validateLoginInfo(String providedUsername, String providedPassword){
+        String fetchedPassword = DatabaseManager.readUserInfo(providedUsername);
+        if (fetchedPassword.isEmpty()){
+            return false;
+        }
+        if (fetchedPassword.equals(providedPassword)){
+            return true;
+        }
+        return false;
+    }
 }
