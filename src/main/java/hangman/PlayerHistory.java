cd923bb43847a4569a2f1c87a68eff0c2a5a881c
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-04 19:44:41 +0330

    implemented on actions, attributes and Initializable

--- src/main/java/hangman/PlayerHistory.java
+++ src/main/java/hangman/PlayerHistory.java
@@ -1,4 +1,22 @@
 package hangman;
 
-public class PlayerHistory {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerHistory implements Initializable {
+    @FXML
+    private TableView history_table;
+
+    @FXML
+    private void onBackButtonClick(){
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
