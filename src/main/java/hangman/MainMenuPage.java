45a12fd2952a1d29a826c9ba7d31d1a5354f91cf
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-06-04 19:38:53 +0330

    implemented onLogoutButtonClick method

--- src/main/java/hangman/MainMenuPage.java
+++ src/main/java/hangman/MainMenuPage.java
@@ -1,4 +1,29 @@
 package hangman;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class MainMenuPage {
+    @FXML
+    private void onLogoutButtonClick() throws IOException {
+        User.logout();
+        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(HangmanApp.class.getResource("login-singup-page.fxml"))));
+        HangmanApp.stage.setScene(scene);
+    }
+    @FXML
+    private void onLeaderboardButtonClick(){
+
+    }
+    @FXML
+    private void onGameHistoryButtonClick(){
+
+    }
+    @FXML
+    private void onNewGameButtonClick(){
+
+    }
 }
